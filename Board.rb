0bcdd92999f8ea7d33aee9903f5c41b9115df3c6
tristanpendergrass & clementpigeon <tristanpendergrass@gmail.com>
2013-08-27 11:32:12 -0400

    reset_pieces sets pawns

--- Board.rb
+++ Board.rb
@@ -31,6 +31,7 @@ class Board
     @board[x][y]
   end
 
+  #fix this(see reset pawns)
   def []=(x, y, value)
     @board[x][y] = value
   end
@@ -39,9 +40,23 @@ class Board
     Array.new(8) { Array.new(8) { nil } }
   end
 
-  def reset_pieces
+  def reset_pieces(player1, player2)
     @board = self.empty_board
-    # ...
+
+    reset_pawns(player1, player2)
+
+  end
+
+  def reset_pawns(player1, player2)
+    (0..7).to_a.each do |i|
+      #change the below notation back
+      self[1, i] = Piece.new(@board, :pawn, player1, [1, i])
+    end
+
+    (0..7).to_a.each do |i|
+      #change the below notation back
+      self[6, i] = Piece.new(@board, :pawn, player2, [6, i])
+    end
   end
 
 end
--- Chess.rb
+++ Chess.rb
@@ -11,10 +11,12 @@ class Chess
     player1 = Player.new(@board, :blue)
     player2 = Player.new(@board, :red)
 
-    p = Piece.new(b, :pawn, player1, nil)
-    king = Piece.new(b, :king, player2, nil)
-    b[0, 0] = p
-    b[0, 1] = king
+    b.reset_pieces(player1, player2)
+
+    # p = Piece.new(b, :pawn, player1, nil)
+    # king = Piece.new(b, :king, player2, nil)
+    # b[0, 0] = p
+    # b[0, 1] = king
     b.display
 
   end
