0b30e29f53966e7af96232175cf4c032abd6a99e
App Academy Student <student@appacademy.io>
2013-08-27 10:49:36 -0400

    Adds the display method for empty board only

--- Board.rb
+++ Board.rb
@@ -1,13 +1,22 @@
-require 'colorize'
 
 class Board
 
   def initialize
-    #
+    @board = self.empty_board
   end
 
   def display
-    puts "board".colorize :red
+    #puts "board".colorize :red
+    puts "   " + ('a'..'h').to_a.join(' ')
+    @board.each_with_index do |row, index|
+      print 8 - index, " "
+      row.each do |square|
+        print '|'
+        print "_" if square.nil?
+      end
+      puts "| #{8 - index}"
+    end
+    puts "   " + ('a'..'h').to_a.join(' ')
   end
 
   def checkmate?
@@ -22,7 +31,16 @@ class Board
     board[x][y] = value
   end
 
+  def empty_board
+    Array.new(8) { Array.new(8) { nil } }
+  end
+
+  def reset_pieces
+    @board = self.empty_board
+    # ...
+  end
 
 end
+
 
 
