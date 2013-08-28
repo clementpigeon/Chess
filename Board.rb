8b48246d973073afd90b52ee9f7d766d2c1c3e63
tristanpendergrass & clementpigeon <tristanpendergrass@gmail.com>
2013-08-28 10:48:26 -0400

    Both sides can be checkmated

--- Board.rb
+++ Board.rb
@@ -28,31 +28,54 @@ class Board
   end
 
   def checkmate?(player1, player2)
-    #false
-    # for blu
     both_kings = @pieces.select { |piece| piece.is_a?(King) }
     player1_king = both_kings.select { |piece| piece.owner.color == :blue }[0]
     player2_king = both_kings.select { |piece| piece.owner.color == :red }[0]
+    kings = [player1_king, player2_king]
 
-    if player1_king.in_check?
-      escapes = player1_king.possible_destinations
-      escapes.select! do |escape|
-        player1_king.valid_move? escape
+    # if player1_king.in_check?
+    #   escapes = player1_king.possible_destinations
+    #   escapes.select! do |escape|
+    #     player1_king.valid_move? escape
+    #   end
+    #
+    #   escapes.reject! do |escape|
+    #     original_location = player1_king.location
+    #
+    #     player1.make_move(player1_king, escape)
+    #     result = player1_king.in_check?
+    #
+    #     player1.make_move(player1_king, original_location)
+    #
+    #     result
+    #   end
+    #
+    #   return escapes.empty?
+    # end
+
+
+    kings.each do |king|
+      if king.in_check?
+        escapes = king.possible_destinations
+        escapes.select! do |escape|
+          king.valid_move? escape
+        end
+
+        escapes.reject! do |escape|
+          original_location = king.location
+
+          king.owner.make_move(king, escape)
+          result = king.in_check?
+
+          king.owner.make_move(king, original_location)
+
+          result
+        end
+
+        return escapes.empty? if escapes.empty?
       end
-
-      escapes.reject! do |escape|
-        original_location = player1_king.location
-
-        player1.make_move(player1_king, escape)
-        result = player1_king.in_check?
-
-        player1.make_move(player1_king, original_location)
-
-        result
-      end
-
-      return escapes.empty?
     end
+
 
     false
   end
